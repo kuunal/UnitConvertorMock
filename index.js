3b05e10f2ed41f8b5e56d016e3593547df974be6
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-16 18:19:52 +0530

    [Kunal] ADD: Fucntionality to Change units

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ unitJson = {
         "feet":12,
         "yard":36
     }
-    , 'Volume':{
+    , 'volume':{
 
-    }, 'Weight':{
+    }, 'weight':{
         "gram":1000,
         "kilogram":1,
         "tonne":1000
@@ -17,7 +17,7 @@ inchToFeet = 1*12;
 feetToInch = 12/1;
 
 function getValuesForDropdown(dropDownId, otherDropDownId){
-    for (element in unitJson['length']){
+    for (element in unitJson[$('#units').val()]){
         if($(otherDropDownId).val() == element){
             continue;
         }
@@ -28,6 +28,15 @@ function getValuesForDropdown(dropDownId, otherDropDownId){
     }
 }
 
+function getValuesForUnits(){
+    unitArray.forEach(element=>
+        $('#units').append(`
+        <option value="${element}"> ${element}</option>
+        `)
+    )
+}
+
+
 function onChange(changedDropDownId, otherDropDownId){
     $(document).on('change',changedDropDownId,function(){
         $(otherDropDownId+' > *').remove();
@@ -36,10 +45,15 @@ function onChange(changedDropDownId, otherDropDownId){
 }
 
 $(document).ready(function(){
+    getValuesForUnits();
     getValuesForDropdown('#unit-1','#unit-2');
     getValuesForDropdown('#unit-2', '#unit-1');
     onChange('#unit-1', '#unit-2');
-    onChange('#unit-2', '#unit-1');
-
+    $(document).on('change','#units',function(){
+        $('#unit-1 > *').remove();
+        $('#unit-2 > *').remove();
+        getValuesForDropdown('#unit-1','#unit-2');
+        getValuesForDropdown('#unit-2','#unit-1');
+    })
 })
 
