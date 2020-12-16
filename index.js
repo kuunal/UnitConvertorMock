d544345f64652bfa28b436a97f92ce998523c2b1
kuunal <kunaldeshmukh2503@gmail.com>
2020-12-16 22:16:56 +0530

    [Kunal] ADD: Unit convertor with mock implmentation

--- index.js
+++ index.js
@@ -1,20 +1,76 @@
 unitArray = ['length', 'volume', 'weight']
 unitJson = {
     'length':{
-        "inch":1,
-        "feet":12,
-        "yard":36
+        "inch":function(value, unit){
+            switch(unit){
+                case "feet": return value / 12;
+                case "yard": return value / 36;
+                default: return 0;
+            }
+        },
+        "feet":function(value, unit){
+            switch(unit){
+                case "inch": return value * 12;
+                case "yard": return value / 3;
+                default: return 0;
+            }
+            
+        },
+        "yard":function(value, unit){
+            switch(unit){
+                case "inch": return value * 36;
+                case "feet": return value * 3;
+                default: return 0;
+            }
+        }
     }
     , 'volume':{
+        "gallon": function(value, unit){
+            switch(unit){
+                case "litre": return value/3.78;
+                case "mililitre":  return value/ 3785.41;
+                default: return 0;
+            }
+        },
+        "litre": function(value, unit){
+            switch(unit){
+                case "gallon": return value * 3.78;
+                case "mililitre":  return value * 1000;
+                default: return 0;
+            }
+        },"mililitre": function(value, unit){
+            switch(unit){
+                case "gallon": return value / 3.785;
+                case "litre":  return value / 1000;
+                default: return 0;
+            }
+        },
 
     }, 'weight':{
-        "gram":1000,
-        "kilogram":1,
-        "tonne":1000
+        "gram":function(value, unit){
+            switch(unit){
+                case "kilogram": return value / 1000;
+                case "tonne":  return value / 1000000;
+                default: return 0;
+            }
+        },
+        "kilogram":function(value, unit){
+            switch(unit){
+                case "gram": return value * 1000;
+                case "tonne":  return value / 1000;
+                default: return 0;
+            }
+        },
+        "tonne":function(value, unit){
+            switch(unit){
+                case "gram": return value * 1000000;
+                case "litre":  return value * 1000;
+                default: return 0;
+            }
+        }
     }
 }
-inchToFeet = 1*12;
-feetToInch = 12/1;
+
 
 function getValuesForDropdown(dropDownId, otherDropDownId){
     for (element in unitJson[$('#units').val()]){
@@ -41,7 +97,16 @@ function onChange(changedDropDownId, otherDropDownId){
     $(document).on('change',changedDropDownId,function(){
         $(otherDropDownId+' > *').remove();
         getValuesForDropdown(otherDropDownId, changedDropDownId)
+        computeValue()
     })
+}
+
+function computeValue(){
+    $('#input-2 > *').remove();
+    value = $('#input-1').val();
+    unit = $('#units').val();
+    console.log($('#unit-1').val())
+    $('#input-2').val(unitJson[unit][$('#unit-1').val()](value, $('#unit-2').val()));
 }
 
 $(document).ready(function(){
@@ -49,11 +114,19 @@ $(document).ready(function(){
     getValuesForDropdown('#unit-1','#unit-2');
     getValuesForDropdown('#unit-2', '#unit-1');
     onChange('#unit-1', '#unit-2');
+    $(document).on('change','#unit-2',function(){
+        computeValue()
+    })
+
     $(document).on('change','#units',function(){
         $('#unit-1 > *').remove();
         $('#unit-2 > *').remove();
         getValuesForDropdown('#unit-1','#unit-2');
         getValuesForDropdown('#unit-2','#unit-1');
+    })
+    $(document).on('keyup','#input-1',function(){
+        console.log("Sss")
+        computeValue();
     })
 })
 
